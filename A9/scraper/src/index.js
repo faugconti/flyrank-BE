faeb129999f5eef047b1c80e8d137f00ebda0ae4
Favio <faugconti@gmail.com>
2026-08-10 20:34:28 -0300

    Stage 1: fetch and cache HTML

--- A9/scraper/src/index.js
+++ A9/scraper/src/index.js
@@ -1,3 +1,11 @@
+import { access, readFile, writeFile, mkdir } from "node:fs/promises";
+import { dirname } from "node:path";
+
+const headers = {
+    'User-Agent': 'FlyRankInternshipA9/1.0 (https://github.com/faugconti/flyrank-BE)'
+}
+
+const CATALOGUE_URL = 'https://books.toscrape.com/';
 
 async function getRobots() {
     try {
@@ -15,4 +23,28 @@ async function getRobots() {
     }
 }
 
-getRobots();
+async function getHtml(url, cacheFile) {
+    try {
+        await access(cacheFile);
+        console.log('CACHE HIT...');
+        return await readFile(cacheFile, "utf-8");
+    }
+    catch (err) {
+        if (err.code !== 'ENOENT') throw err;
+    }
+    console.log('FETCH..');
+    const response = await fetch(url, {
+        headers,
+        signal: AbortSignal.timeout(5000)
+    });
+
+    if (response.status !== 200)
+        throw new Error(`HTTP ${response.status}`);
+
+    const html = await response.text();
+    await mkdir(dirname(cacheFile), { recursive: true });
+    await writeFile(cacheFile, html, 'utf-8');
+    return html;
+}
+
+await getHtml(CATALOGUE_URL, 'cache/catalogue-page-1.html');
